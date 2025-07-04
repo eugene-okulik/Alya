0f092f85113ab4eb4701c89f3e456ac9b1c259f4
Eugene Zhidok <eugene.zhidok@intexsoft.by>
2025-07-05 00:07:46 +0300

    homework 5 is done

---
+++ homework/alya_zhydok/lesson_5/file_3.py
@@ -0,0 +1,8 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+a, b, c = students
+d, e, f = subjects
+
+my_text = f'Students {a}, {b}, {c} study these subjects: {d}, {e}, {f}'
+print(my_text)
