41a1012a7eb9c3ce93557d3f0256864b38d9e1d3
Eugene Zhidok <eugene.zhidok@intexsoft.by>
2025-07-05 00:15:57 +0300

    fix file_1 second time

--- homework/alya_zhydok/lesson_5/file_1.py
+++ homework/alya_zhydok/lesson_5/file_1.py
@@ -1,6 +1,4 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
-print(
-    'name: ' + name + ', last_name: ' + last_name + ', city: ' + city + ', phone: ' + phone + ', country: ' + country
-    )
+print('name: ' + name + ', last_name: ' + last_name + ', city: ' + city + ', phone: ' + phone + ', country: ' + country)
