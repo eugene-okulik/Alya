26cb06f7fe81b780b67ff63777ca274f31dcab6e
Eugene Zhidok <eugene.zhidok@intexsoft.by>
2025-07-07 10:01:08 +0300

    use join in file3

--- homework/alya_zhydok/lesson_5/file_3.py
+++ homework/alya_zhydok/lesson_5/file_3.py
@@ -1,8 +1,7 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-a, b, c = students
-d, e, f = subjects
 
-my_text = f'Students {a}, {b}, {c} study these subjects: {d}, {e}, {f}'
-print(my_text)
+students = ', '.join(students)
+subjects = ', '.join(subjects)
+print('Students ', students, 'study these subjects:', subjects)
