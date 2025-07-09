e1d49290463b0f34f9722e38e3255cc7d8e54a08
Eugene Zhidok <eugene.zhidok@intexsoft.by>
2025-07-09 20:26:03 +0300

    fix result in file1

--- homework/alya_zhydok/lesson_6/file_1.py
+++ homework/alya_zhydok/lesson_6/file_1.py
@@ -1,13 +1,17 @@
 text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, \
     dignissim vitae libero'
+
 words = text.split()
 # print(words)
+fin_words = []
 for word in words:
     if word.endswith(','):
         word = word.replace(',', 'ing,')
-        print(word)
+        fin_words.append(word)
     elif word.endswith('.'):
         word = word.replace('.', 'ing.')
-        print(word)
+        fin_words.append(word)
     else:
-        print(word + 'ing')
+        word = word + 'ing'
+        fin_words.append(word)
+print(' '.join(fin_words))
