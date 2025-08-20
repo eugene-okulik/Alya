75b7584ab843b4b97f45bc307b0d798096d10a00
Eugene Zhidok <eugene.zhidok@intexsoft.by>
2025-08-20 12:12:48 +0300

    edit function in file4

--- homework/alya_zhydok/lesson_7/file_3.py
+++ homework/alya_zhydok/lesson_7/file_3.py
@@ -1,29 +1,14 @@
-result_1 = 'результат операции: 42'
+def extract_number(s: str) -> int:
+    return int(s.split(':')[-1].strip())
+
+
+result_1 = "результат операции: 42"
 result_2 = 'результат операции: 54'
 result_3 = 'результат работы программы: 209'
 result_4 = 'результат работы программы: 2'
+main_number = 10
 
-
-# print(result_1.index(': '))
-# print(result_1[20:])
-a = result_1.index(': ') + 2
-a_1 = int(result_1[a:])
-
-b = result_2.index(': ') + 2
-b_1 = int(result_2[b:])
-
-c = result_3.index(': ') + 2
-c_1 = int(result_3[c:])
-
-d = result_4.index(': ') + 2
-d_1 = int(result_4[c:])
-
-
-def calc(numb):
-    print(numb + 10)
-
-
-calc(a_1)
-calc(b_1)
-calc(c_1)
-calc(d_1)
+print(extract_number(result_1) + main_number)
+print(extract_number(result_2) + main_number)
+print(extract_number(result_3) + main_number)
+print(extract_number(result_4) + main_number)
