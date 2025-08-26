186cb0267b0e9ae748d4b71051dcd1f69e15234f
Eugene Zhidok <eugene.zhidok@intexsoft.by>
2025-08-26 12:10:34 +0300

    clousing bracket in 2file2

--- homework/alya_zhydok/lesson_9/file_2.py
+++ homework/alya_zhydok/lesson_9/file_2.py
@@ -1,16 +1,16 @@
 temperatures = [
     20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
     22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23
-    ]
+]
 
 hot_days = list(filter(lambda temp: temp > 28, temperatures))
 print('Жаркие дни:', hot_days)
 
 max_temp = max(hot_days)
-print("Самая высокая температура:", max_temp)
+print('Самая высокая температура:', max_temp)
 
 min_temp = min(hot_days)
-print("Самая низкая температура:", min_temp)
+print('Самая низкая температура:', min_temp)
 
 avg_temp = sum(hot_days) / len(hot_days)
-print("Средняя температура:", avg_temp)
+print('Средняя температура:', avg_temp)
